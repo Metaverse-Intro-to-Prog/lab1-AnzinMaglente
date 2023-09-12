98239d5d44f9a781d61c78d3a1fc0d2a901e5be2
Anzin-Maglente <144773742+AnzinMaglente@users.noreply.github.com>
2023-09-12 19:40:12 +0400

    Add files via upload

---
+++ Chapter 4- Control Flow/Exercises/Chapter 4/Exercise 1 (True).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 18:21:04 2023
+
+@author: Anzin R. Maglente
+"""
+
+"""
+Alien color, make a variable named "alien color" and check if it is green, if
+true the player gets 5 points, if not the player loses the game. Show both outcomes
+"""
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You earned 5 points")
